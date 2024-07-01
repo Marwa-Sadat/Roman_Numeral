df9867888856c8c0dbacbdd162f2bd7af32f921e
unknown <marwasadat735@gmail.com>
2024-07-01 21:52:30 +0330

    styles changes

--- script.js
+++ script.js
@@ -20,3 +20,30 @@ document.getElementById('convert-btn').onclick = function () {
 
   outputDiv.textContent = convertToRoman(number);
 };
+
+function convertToRoman(num) {
+  const romanNumeralMap = [
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' }
+  ];
+
+  let result = '';
+  for (const { value, numeral } of romanNumeralMap) {
+    while (num >= value) {
+      result += numeral;
+      num -= value;
+    }
+  }
+  return result;
+}
