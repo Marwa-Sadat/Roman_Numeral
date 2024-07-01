7f722cde8c7d0987bd147a4bd6bd3ca02d1806cb
unknown <marwasadat735@gmail.com>
2024-07-01 21:56:32 +0330

    styles changes

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+function convertToRoman(num) {
+  const romanNumeralMap = [
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' },
+  ];
+
+  return romanNumeralMap.reduce((acc, { value, numeral }) => {
+    while (num >= value) {
+      acc += numeral;
+      num -= value;
+    }
+    return acc;
+  }, '');
+}
+
 document.getElementById('convert-btn').onclick = function () {
   const numberInput = document.getElementById('number').value;
   const outputDiv = document.getElementById('output');
@@ -20,30 +46,3 @@ document.getElementById('convert-btn').onclick = function () {
 
   outputDiv.textContent = convertToRoman(number);
 };
-
-function convertToRoman(num) {
-  const romanNumeralMap = [
-    { value: 1000, numeral: 'M' },
-    { value: 900, numeral: 'CM' },
-    { value: 500, numeral: 'D' },
-    { value: 400, numeral: 'CD' },
-    { value: 100, numeral: 'C' },
-    { value: 90, numeral: 'XC' },
-    { value: 50, numeral: 'L' },
-    { value: 40, numeral: 'XL' },
-    { value: 10, numeral: 'X' },
-    { value: 9, numeral: 'IX' },
-    { value: 5, numeral: 'V' },
-    { value: 4, numeral: 'IV' },
-    { value: 1, numeral: 'I' }
-  ];
-
-  let result = '';
-  for (const { value, numeral } of romanNumeralMap) {
-    while (num >= value) {
-      result += numeral;
-      num -= value;
-    }
-  }
-  return result;
-}
